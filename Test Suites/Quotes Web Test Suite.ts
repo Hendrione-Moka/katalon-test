0f1dbe29b939bc0466175d0c8cf13d239f449b71
Hendri Antomy <hendri.antomy@mokapos.com>
2022-04-12 15:59:40 +0700

    modify test suite

--- Test Suites/Quotes Web Test Suite.ts
+++ Test Suites/Quotes Web Test Suite.ts
@@ -53,4 +53,64 @@
       <isRun>true</isRun>
       <testCaseId>Test Cases/Table View/Hover Blue Button to Show Table</testCaseId>
    </testCaseLink>
+   <testCaseLink>
+      <guid>674abd9d-f4af-48d9-b948-b5f4bfd2f3d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Add Quote/Add Quote With Color Cyan</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>7edc948a-2f12-4dde-a172-7c956c29aef4</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>7d119708-3868-4402-ae46-2137d79a2b55</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>01715318-b197-49f4-8d71-8dec9552bfb8</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2f4c9502-0fd2-43d1-8235-29f2161b22fe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Add Quote/Add Quote With Color Yellow</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d37fcff3-7fd6-42f7-a8b7-099437ef1d26</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Add Quote/Click Powered By Link</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5e08c799-01c5-41fd-b970-7bc662d87294</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Table View/Hover Cyan Button to Hide Table</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>473d65d7-eda5-4a55-afd1-4c3c00ea0fb8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Table View/Click Yellow Button to Show Table (Wait 10 sec)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4241e3b3-3f10-4345-b474-33b96b58b9b0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Table View/Hover Blue Button to Show Table</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9efdf7ad-faad-4e06-9054-ce1ca52e135f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/API/API Reqres</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
